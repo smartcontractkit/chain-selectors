3b8990c3f2a58a296496b991407bb8362af658a5
Mateusz Sekara <mateusz.sekara@gmail.com>
2023-08-17 20:03:19 +0200

    Read data from yml file instead of hardcoding in Go (#1)

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # CCIP chain selectors
 
-CCIP uses its own set of chain selectors represented by uint64 to identify blockchains. This repository contains a mapping between the CCIP chain identifiers (`chainSelectorId`) and the chain identifiers used by the blockchains themselves (`chainId`).
+CCIP uses its own set of chain selectors represented by uint64 to identify blockchains. This repository contains a
+mapping between the CCIP chain identifiers (`chainSelectorId`) and the chain identifiers used by the blockchains
+themselves (`chainId`).
 
-Please refer to the [official documentation](https://docs.chain.link/ccip/supported-networks) to learn more about supported networks and their selectors.
+Please refer to the [official documentation](https://docs.chain.link/ccip/supported-networks) to learn more about
+supported networks and their selectors.
 
 ### Installation
 
@@ -18,15 +21,24 @@ import (
 func main() {
     // Getting selector based on ChainId
     selector, err := selectors.SelectorFromChainId(420)
-    
+
     // Getting ChainId based on ChainSelector
     chainId, err := selectors.ChainIdFromSelector(2664363617261496610)
-    
+
     // Accessing mapping directly
     lookupChainId := uint64(1337)
-    if chainSelector, exists := selectors.EvmChainIdToChainSelector[lookupChainId]; exists {
+    if chainSelector, exists := selectors.EvmChainIdToChainSelector()[lookupChainId]; exists {
         fmt.Println("Found chain selector for chain", lookupChainId, ":", chainSelector)
     }
 }
-
 ```
+
+### Contributing
+
+Any new chains and selectors should be always added to [selectors.yml](selectors.yml) and client libraries should load
+details from this file. This ensures that all client libraries are in sync and use the same mapping.
+
+If you need a support for a new language, please open a PR with the following changes:
+- Library codebase is in a separate directory
+- Library uses selectors.yml as a source of truth
+- Proper Github workflow is present to make sure code compiles and tests pass
--- selectors.go
+++ selectors.go
@@ -1,30 +1,40 @@
-package selectors
+package ccip_chain_selectors
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
 
-var EvmChainIdToChainSelector = map[uint64]uint64{
-	// Testnets
-	97:       13264668187771770619, // BSC Testnet
-	420:      2664363617261496610,  // Optimism Goerli
-	1337:     3379446385462418246,  // Dev network
-	43113:    14767482510784806043, // Avalanche Fuji
-	84531:    5790810961207155433,  // BASE Goerli
-	80001:    12532609583862916517, // Polygon Mumbai
-	421613:   6101244977088475029,  // Arbitrum Goerli
-	11155111: 16015286601757825753, // Sepolia
+	"gopkg.in/yaml.v3"
+)
 
-	// Mainnets
-	1:     5009297550715157269,  // Ethereum
-	10:    3734403246176062136,  // Optimism
-	56:    11344663589394136015, // BSC
-	137:   4051577828743386545,  // Polygon
-	8453:  15971525489660198786, // BASE
-	42161: 4949039107694359620,  // Arbitrum
-	43114: 6433500567565415381,  // Avalanche
+//go:embed selectors.yml
+var selectorsYml []byte
+var evmChainIdToChainSelector = parseYml()
+
+func parseYml() map[uint64]uint64 {
+	type ymlData struct {
+		Selectors map[uint64]uint64 `yaml:"selectors"`
+	}
+
+	var data ymlData
+	err := yaml.Unmarshal(selectorsYml, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	return data.Selectors
+}
+
+func EvmChainIdToChainSelector() map[uint64]uint64 {
+	copyMap := make(map[uint64]uint64, len(evmChainIdToChainSelector))
+	for k, v := range evmChainIdToChainSelector {
+		copyMap[k] = v
+	}
+	return copyMap
 }
 
 func ChainIdFromSelector(chainSelectorId uint64) (uint64, error) {
-	for k, v := range EvmChainIdToChainSelector {
+	for k, v := range evmChainIdToChainSelector {
 		if v == chainSelectorId {
 			return k, nil
 		}
@@ -33,7 +43,7 @@ func ChainIdFromSelector(chainSelectorId uint64) (uint64, error) {
 }
 
 func SelectorFromChainId(chainId uint64) (uint64, error) {
-	if chainSelectorId, exist := EvmChainIdToChainSelector[chainId]; exist {
+	if chainSelectorId, exist := evmChainIdToChainSelector[chainId]; exist {
 		return chainSelectorId, nil
 	}
 	return 0, fmt.Errorf("chain selector not found for chain %d", chainId)
