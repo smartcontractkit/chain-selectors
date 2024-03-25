d97c4185c457a8debbd86f9a673e8726e136468e
Kashif Siddiqui <kashif.siddiqui@smartcontract.com>
2024-03-25 15:48:49 +0900

    Add Polygon Amoy support

--- generated_chains.go
+++ generated_chains.go
@@ -55,6 +55,7 @@ var (
 	KAVA_MAINNET                             = Chain{EvmChainID: 2222, Selector: 7550000543357438061, Name: "kava-mainnet"}
 	KAVA_TESTNET                             = Chain{EvmChainID: 2221, Selector: 2110537777356199208, Name: "kava-testnet"}
 	POLYGON_MAINNET                          = Chain{EvmChainID: 137, Selector: 4051577828743386545, Name: "polygon-mainnet"}
+	POLYGON_TESTNET_AMOY                     = Chain{EvmChainID: 80002, Selector: 16281711391670634445, Name: "polygon-testnet-amoy"}
 	POLYGON_TESTNET_MUMBAI                   = Chain{EvmChainID: 80001, Selector: 12532609583862916517, Name: "polygon-testnet-mumbai"}
 	TEST_1000                                = Chain{EvmChainID: 1000, Selector: 11787463284727550157, Name: "1000"}
 	TEST_2337                                = Chain{EvmChainID: 2337, Selector: 12922642891491394802, Name: "2337"}
@@ -130,6 +131,7 @@ var ALL = []Chain{
 	KAVA_MAINNET,
 	KAVA_TESTNET,
 	POLYGON_MAINNET,
+	POLYGON_TESTNET_AMOY,
 	POLYGON_TESTNET_MUMBAI,
 	TEST_1000,
 	TEST_2337,
