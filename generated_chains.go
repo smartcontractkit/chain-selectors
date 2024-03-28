47b786b26655f2c9708153a415d08942d812fe5c
Finley Decker <finleydecker@gmail.com>
2024-03-27 14:35:41 -1000

    add metis

--- generated_chains.go
+++ generated_chains.go
@@ -21,6 +21,7 @@ var (
 	CELO_TESTNET_ALFAJORES                   = Chain{EvmChainID: 44787, Selector: 3552045678561919002, Name: "celo-testnet-alfajores"}
 	CRONOS_TESTNET                           = Chain{EvmChainID: 338, Selector: 2995292832068775165, Name: "cronos-testnet"}
 	ETHEREUM_MAINNET                         = Chain{EvmChainID: 1, Selector: 5009297550715157269, Name: "ethereum-mainnet"}
+	ETHEREUM_MAINNET_ANDROMEDA_METIS_1       = Chain{EvmChainID: 1088, Selector: 8805746078405598895, Name: "ethereum-mainnet-andromeda-metis-1"}
 	ETHEREUM_MAINNET_ARBITRUM_1              = Chain{EvmChainID: 42161, Selector: 4949039107694359620, Name: "ethereum-mainnet-arbitrum-1"}
 	ETHEREUM_MAINNET_BASE_1                  = Chain{EvmChainID: 8453, Selector: 15971525489660198786, Name: "ethereum-mainnet-base-1"}
 	ETHEREUM_MAINNET_KROMA_1                 = Chain{EvmChainID: 255, Selector: 3719320017875267166, Name: "ethereum-mainnet-kroma-1"}
@@ -43,6 +44,7 @@ var (
 	ETHEREUM_TESTNET_SEPOLIA_KROMA_1         = Chain{EvmChainID: 2358, Selector: 5990477251245693094, Name: "ethereum-testnet-sepolia-kroma-1"}
 	ETHEREUM_TESTNET_SEPOLIA_LISK_1          = Chain{EvmChainID: 4202, Selector: 5298399861320400553, Name: "ethereum-testnet-sepolia-lisk-1"}
 	ETHEREUM_TESTNET_SEPOLIA_MANTLE_1        = Chain{EvmChainID: 5003, Selector: 8236463271206331221, Name: "ethereum-testnet-sepolia-mantle-1"}
+	ETHEREUM_TESTNET_SEPOLIA_METIS_1         = Chain{EvmChainID: 59902, Selector: 3777822886988675105, Name: "ethereum-testnet-sepolia-metis-1"}
 	ETHEREUM_TESTNET_SEPOLIA_OPTIMISM_1      = Chain{EvmChainID: 11155420, Selector: 5224473277236331295, Name: "ethereum-testnet-sepolia-optimism-1"}
 	ETHEREUM_TESTNET_SEPOLIA_POLYGON_ZKEVM_1 = Chain{EvmChainID: 2442, Selector: 1654667687261492630, Name: "ethereum-testnet-sepolia-polygon-zkevm-1"}
 	ETHEREUM_TESTNET_SEPOLIA_SCROLL_1        = Chain{EvmChainID: 534351, Selector: 2279865765895943307, Name: "ethereum-testnet-sepolia-scroll-1"}
@@ -97,6 +99,7 @@ var ALL = []Chain{
 	CELO_TESTNET_ALFAJORES,
 	CRONOS_TESTNET,
 	ETHEREUM_MAINNET,
+	ETHEREUM_MAINNET_ANDROMEDA_METIS_1,
 	ETHEREUM_MAINNET_ARBITRUM_1,
 	ETHEREUM_MAINNET_BASE_1,
 	ETHEREUM_MAINNET_KROMA_1,
@@ -119,6 +122,7 @@ var ALL = []Chain{
 	ETHEREUM_TESTNET_SEPOLIA_KROMA_1,
 	ETHEREUM_TESTNET_SEPOLIA_LISK_1,
 	ETHEREUM_TESTNET_SEPOLIA_MANTLE_1,
+	ETHEREUM_TESTNET_SEPOLIA_METIS_1,
 	ETHEREUM_TESTNET_SEPOLIA_OPTIMISM_1,
 	ETHEREUM_TESTNET_SEPOLIA_POLYGON_ZKEVM_1,
 	ETHEREUM_TESTNET_SEPOLIA_SCROLL_1,
