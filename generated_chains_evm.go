0d61ab6b9b965fa8df0f32aad3871cd517fa2782
Nahuel Batista <nahuel.batista@smartcontract.com>
2025-03-11 11:58:33 -0300

    Fixes MegaETH selector

--- generated_chains_evm.go
+++ generated_chains_evm.go
@@ -134,6 +134,7 @@ var (
 	KAVA_TESTNET                                   = Chain{EvmChainID: 2221, Selector: 2110537777356199208, Name: "kava-testnet"}
 	KUSAMA_MAINNET_MOONRIVER                       = Chain{EvmChainID: 1285, Selector: 1355020143337428062, Name: "kusama-mainnet-moonriver"}
 	LENS_MAINNET                                   = Chain{EvmChainID: 232, Selector: 5608378062013572713, Name: "lens-mainnet"}
+	MEGAETH_TESTNET                                = Chain{EvmChainID: 6342, Selector: 2443239559770384419, Name: "megaeth-testnet"}
 	MIND_MAINNET                                   = Chain{EvmChainID: 228, Selector: 11690709103138290329, Name: "mind-mainnet"}
 	MIND_TESTNET                                   = Chain{EvmChainID: 192940, Selector: 7189150270347329685, Name: "mind-testnet"}
 	MONAD_TESTNET                                  = Chain{EvmChainID: 10143, Selector: 2183018362218727504, Name: "monad-testnet"}
@@ -410,6 +411,7 @@ var ALL = []Chain{
 	KAVA_TESTNET,
 	KUSAMA_MAINNET_MOONRIVER,
 	LENS_MAINNET,
+	MEGAETH_TESTNET,
 	MIND_MAINNET,
 	MIND_TESTNET,
 	MONAD_TESTNET,
