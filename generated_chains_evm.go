2ab2ecaaab1eac9609b09cb7792dad3f3c253207
flodesi <jaden.foldesi@smartcontract.com>
2025-05-02 09:45:14 -0400

    nit

--- generated_chains_evm.go
+++ generated_chains_evm.go
@@ -66,7 +66,7 @@ var (
 	ETHEREUM_MAINNET_HASHKEY_1                     = Chain{EvmChainID: 177, Selector: 7613811247471741961, Name: "ethereum-mainnet-hashkey-1"}
 	ETHEREUM_MAINNET_IMMUTABLE_ZKEVM_1             = Chain{EvmChainID: 13371, Selector: 1237925231416731909, Name: "ethereum-mainnet-immutable-zkevm-1"}
 	ETHEREUM_MAINNET_INK_1                         = Chain{EvmChainID: 57073, Selector: 3461204551265785888, Name: "ethereum-mainnet-ink-1"}
-	ETHEREUM_MAINNET_JANCTION_1                    = Chain{EvmChainID: 678, Selector: 9107126442626377432, Name: "ethereum-mainnet-janction-1"}
+	ETHEREUM_MAINNET_JANCTION                      = Chain{EvmChainID: 678, Selector: 9107126442626377432, Name: "ethereum-mainnet-janction"}
 	ETHEREUM_MAINNET_KROMA_1                       = Chain{EvmChainID: 255, Selector: 3719320017875267166, Name: "ethereum-mainnet-kroma-1"}
 	ETHEREUM_MAINNET_LINEA_1                       = Chain{EvmChainID: 59144, Selector: 4627098889531055414, Name: "ethereum-mainnet-linea-1"}
 	ETHEREUM_MAINNET_MANTLE_1                      = Chain{EvmChainID: 5000, Selector: 1556008542357238666, Name: "ethereum-mainnet-mantle-1"}
@@ -375,7 +375,7 @@ var ALL = []Chain{
 	ETHEREUM_MAINNET_HASHKEY_1,
 	ETHEREUM_MAINNET_IMMUTABLE_ZKEVM_1,
 	ETHEREUM_MAINNET_INK_1,
-	ETHEREUM_MAINNET_JANCTION_1,
+	ETHEREUM_MAINNET_JANCTION,
 	ETHEREUM_MAINNET_KROMA_1,
 	ETHEREUM_MAINNET_LINEA_1,
 	ETHEREUM_MAINNET_MANTLE_1,
