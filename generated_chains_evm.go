629c35a77da65d9f9e8250b13ee64d57fa640116
flodesi <jaden.foldesi@smartcontract.com>
2025-05-23 11:49:28 -0400

    fixnaming2

--- generated_chains_evm.go
+++ generated_chains_evm.go
@@ -182,8 +182,8 @@ var (
 	POLYGON_MAINNET                                = Chain{EvmChainID: 137, Selector: 4051577828743386545, Name: "polygon-mainnet"}
 	POLYGON_MAINNET_KATANA                         = Chain{EvmChainID: 747474, Selector: 2459028469735686113, Name: "polygon-mainnet-katana"}
 	POLYGON_TESTNET_AMOY                           = Chain{EvmChainID: 80002, Selector: 16281711391670634445, Name: "polygon-testnet-amoy"}
-	POLYGON_TESTNET_KATANA_TATARA                  = Chain{EvmChainID: 129399, Selector: 9090863410735740267, Name: "polygon-testnet-katana-tatara"}
 	POLYGON_TESTNET_MUMBAI                         = Chain{EvmChainID: 80001, Selector: 12532609583862916517, Name: "polygon-testnet-mumbai"}
+	POLYGON_TESTNET_TATARA                         = Chain{EvmChainID: 129399, Selector: 9090863410735740267, Name: "polygon-testnet-tatara"}
 	PRIVATE_TESTNET_MICA                           = Chain{EvmChainID: 424242, Selector: 4489326297382772450, Name: "private-testnet-mica"}
 	PRIVATE_TESTNET_OPALA                          = Chain{EvmChainID: 45439, Selector: 8446413392851542429, Name: "private-testnet-opala"}
 	RONIN_MAINNET                                  = Chain{EvmChainID: 2020, Selector: 6916147374840168594, Name: "ronin-mainnet"}
@@ -499,8 +499,8 @@ var ALL = []Chain{
 	POLYGON_MAINNET,
 	POLYGON_MAINNET_KATANA,
 	POLYGON_TESTNET_AMOY,
-	POLYGON_TESTNET_KATANA_TATARA,
 	POLYGON_TESTNET_MUMBAI,
+	POLYGON_TESTNET_TATARA,
 	PRIVATE_TESTNET_MICA,
 	PRIVATE_TESTNET_OPALA,
 	RONIN_MAINNET,
