62cbb8f10a854fe6d1c2935fb583d3e1cb4ffc18
flodesi <jaden.foldesi@smartcontract.com>
2026-09-03 10:36:44 -0400

    add testnet instance name

--- generated_chains_evm.go
+++ generated_chains_evm.go
@@ -163,7 +163,7 @@ var (
 	GNOSIS_CHAIN_MAINNET                           = Chain{EvmChainID: 100, Selector: 465200170687744372, Name: "gnosis_chain-mainnet", NetworkType: NetworkTypeMainnet}
 	GNOSIS_CHAIN_TESTNET_CHIADO                    = Chain{EvmChainID: 10200, Selector: 8871595565390010547, Name: "gnosis_chain-testnet-chiado", NetworkType: NetworkTypeTestnet}
 	GRAVITY_MAINNET                                = Chain{EvmChainID: 127001, Selector: 2988178761202034333, Name: "gravity-mainnet", NetworkType: NetworkTypeMainnet}
-	GRAVITY_TESTNET                                = Chain{EvmChainID: 7771625, Selector: 9181335567442030426, Name: "gravity-testnet", NetworkType: NetworkTypeTestnet}
+	GRAVITY_TESTNET_LONGEVITY                      = Chain{EvmChainID: 7771625, Selector: 9181335567442030426, Name: "gravity-testnet-longevity", NetworkType: NetworkTypeTestnet}
 	HEDERA_MAINNET                                 = Chain{EvmChainID: 295, Selector: 3229138320728879060, Name: "hedera-mainnet", NetworkType: NetworkTypeMainnet}
 	HEDERA_TESTNET                                 = Chain{EvmChainID: 296, Selector: 222782988166878823, Name: "hedera-testnet", NetworkType: NetworkTypeTestnet}
 	HEMI_MAINNET                                   = Chain{EvmChainID: 43111, Selector: 1804312132722180201, Name: "hemi-mainnet", NetworkType: NetworkTypeMainnet}
@@ -553,7 +553,7 @@ var ALL = []Chain{
 	GNOSIS_CHAIN_MAINNET,
 	GNOSIS_CHAIN_TESTNET_CHIADO,
 	GRAVITY_MAINNET,
-	GRAVITY_TESTNET,
+	GRAVITY_TESTNET_LONGEVITY,
 	HEDERA_MAINNET,
 	HEDERA_TESTNET,
 	HEMI_MAINNET,
